98350f76a8c797f1fdd644aa164f8699be62fcdd
laraoye <adekolayemi25@gmail.com>
2024-11-10 06:55:04 +0100

    update login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,56 +1,142 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
 
-        <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
+    <!-- Session Status -->
+    <x-auth-session-status class="mb-4" :status="session('status')" />
+<!--
+=========================================================
+* Corporate UI - v1.0.0
+=========================================================
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+* Product Page: https://www.creative-tim.com/product/corporate-ui
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://www.creative-tim.com/license)
+* Coded by Creative Tim
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
+=========================================================
 
-            <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+-->
+<!DOCTYPE html>
+<html lang="en">
 
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="../assets/img/favicon.png">
+  <title>
+    Great Travels
+  </title>
+  <!--     Fonts and icons     -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700|Noto+Sans:300,400,500,600,700,800|PT+Mono:300,400,500,600,700" rel="stylesheet" />
+  <!-- Nucleo Icons -->
+  <link href="../assets/css/nucleo-icons.css" rel="stylesheet" />
+  <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
+  <!-- Font Awesome Icons -->
+  <script src="https://kit.fontawesome.com/349ee9c857.js" crossorigin="anonymous"></script>
+  <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
+  <!-- CSS Files -->
+  <link id="pagestyle" href="../assets/css/corporate-ui-dashboard.css?v=1.0.0" rel="stylesheet" />
+</head>
+
+<body class="">
+  <main class="main-content  mt-0">
+    <section>
+      <div class="page-header min-vh-100">
+        <div class="container">
+          <div class="row">
+            <div class="col-xl-4 col-md-6 d-flex flex-column mx-auto">
+              <div class="card card-plain mt-8">
+                <div class="card-header pb-0 text-left bg-transparent">
+                  <h3 class="font-weight-black text-dark display-6">Welcome back</h3>
+                  <p class="mb-0">Welcome back! Please enter your details.</p>
+                </div>
+                <div class="card-body">
+                  <form method="POST" action="{{ route('login') }}">
+        @csrf
+                    <div class="mb-3">
+                    <div class="mb-3">
+    </div>
+
+<div class="mb-3">
+    <label for="email">Email Address</label>
+    <x-input-label for="email" :value="__('Email')" />
+    <x-text-input id="email" class="block mt-1 w-full form-control" type="email" placeholder="Enter your email address" aria-label="Email" aria-describedby="email-addon" name="email" :value="old('email')" required autofocus autocomplete="username" />
+    <x-input-error :messages="$errors->get('email')" class="mt-2" />
+</div>
+
+<div class="mb-3">
+    <label for="password">Password</label>
+    <x-input-label for="password" :value="__('Password')" />
+    <x-text-input id="password" class="block mt-1 w-full form-control" type="password" placeholder="Enter password" aria-label="Password" aria-describedby="password-addon" name="password" required autocomplete="current-password" />
+    <x-input-error :messages="$errors->get('password')" class="mt-2" />
+</div>
+
+<div class="d-flex align-items-center mb-3">
+    <div class="form-check form-check-info text-left">
+        <input class="form-check-input" type="checkbox" id="remember_me" name="remember">
+        <label class="font-weight-normal text-dark" for="remember_me">
+            Remember for 14 days
+            <span class="ml-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
+        </label>
+    </div>
+    @if (Route::has('password.request'))
+        <a href="{{ route('password.request') }}" class="text-xs font-weight-bold ms-auto">
+            {{ __('Forgot your password?') }}
+        </a>
+    @endif
+</div>
+
+<div class="text-center">
+    <!-- Log in Button -->
+    <x-primary-button class="btn btn-dark w-100 mt-4 mb-3">
+        {{ __('Log in') }}
+    </x-primary-button>
+</div>
+                    </div>
+                  </form>
+                </div>
+                <div class="card-footer text-center pt-0 px-lg-2 px-1">
+                  <p class="mb-4 text-xs mx-auto">
+                    Don't have an account?
+                    <a href="javascript:;" class="text-dark font-weight-bold">Sign up</a>
+                  </p>
+                </div>
+              </div>
             </div>
-
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
-
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
+            <div class="col-md-6">
+              <div class="position-absolute w-40 top-0 end-0 h-100 d-md-block d-none">
+                <div class="oblique-image position-absolute fixed-top ms-auto h-100 z-index-0 bg-cover ms-n8" style="background-image:url('../assets/img/image-sign-in.jpg')">
+                  <div class="blur mt-12 p-4 text-center border border-white border-radius-md position-absolute fixed-bottom m-4">
+                    <h2 class="mt-3 text-dark font-weight-bold">Sign in to your admin dashboard</h2>
+                    <h6 class="text-dark text-sm mt-5">Copyright Great Travels</h6>
+                  </div>
+                </div>
+              </div>
             </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+  <!--   Core JS Files   -->
+  <script src="../assets/js/core/popper.min.js"></script>
+  <script src="../assets/js/core/bootstrap.min.js"></script>
+  <script src="../assets/js/plugins/perfect-scrollbar.min.js"></script>
+  <script src="../assets/js/plugins/smooth-scrollbar.min.js"></script>
+  <script>
+    var win = navigator.platform.indexOf('Win') > -1;
+    if (win && document.querySelector('#sidenav-scrollbar')) {
+      var options = {
+        damping: '0.5'
+      }
+      Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
+    }
+  </script>
+  <!-- Github buttons -->
+  <script async defer src="https://buttons.github.io/buttons.js"></script>
+  <!-- Control Center for Corporate UI Dashboard: parallax effects, scripts for the example pages etc -->
+  <script src="../assets/js/corporate-ui-dashboard.min.js?v=1.0.0"></script>
+</body>
 
-            <!-- Remember Me -->
-            <div class="block mt-4">
-                <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
+</html>
 
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-button class="ml-3">
-                    {{ __('Log in') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
